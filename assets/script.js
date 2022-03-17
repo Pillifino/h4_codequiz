97d205e6df900ec12801d1e307071147fd710975
Pillifino <dinodizon073100@gmail.com>
2022-03-16 18:44:13 -0700

    Added Function to change text in element with class namme of question-container

--- assets/script.js
+++ assets/script.js
@@ -1,38 +1,35 @@
 // Object Question 1
 var questionObject = {
     questionText: "what is the answer to 1?",
-    answer1: "This is the answer",
-    answer2: "This is not the answer",
+    answerArray: [],
 };
 // Object Question 2
 var questionObject2 = {
     questionText: "what is the answer to 2?",
-    answer1: "This is the answer",
-    answer2: "This is not the answer",
-    answer3: "This is not the answer",
-    answer4: "This is not the answer",
-    answer5: "This is not the answer",
+    answerArray:[],
 }
 // Object Question 3
 var questionObject3 = {
     questionText: "what is the answer to 3?",
-    answer1: "This is not the answer",
-    answer2: "This is the answer",
-    answer3: "This is not the answer",
-    answer4: "This is not the answer",
+    answerArray:[],
 }
 // Object Question 4
 var questionObject4 = {
     questionText: "what is the answer to 4?",
-    answer1: "This is not the answer",
-    answer2: "This is not the answer",
-    answer3: "This is the answer",
-    answer4: "This is not the answer",
+    answerArray:[],
 }
 // Object Question 5
 var questionObject5 = {
     questionText: "what is the answer to 5?",
-    answer1: "This is not the answer",
-    answer2: "This is not the answer",
-    answer3: "This is the answer",
+    answerArray:[],
 }
+
+//creating query selectors
+var questionContainer = document.querySelector(".question-container");
+var startButton = document.querySelector(".start");
+
+//Change text of question-container class text to object 1's property "questionText"
+startButton.addEventListener('click' , function(event) {
+    //
+    questionContainer.innerHTML = questionObject.questionText;
+})

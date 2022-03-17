fabd64309a3cb005927e5f2b810b45afb5bbcc3f
Pillifino <dinodizon073100@gmail.com>
2022-03-17 01:43:06 -0700

    Created an object of arrays that will allow me to iterate between questions and have more dynamic code

--- assets/script.js
+++ assets/script.js
@@ -1,35 +1,41 @@
-// Object Question 1
-var questionObject = {
-    questionText: "what is the answer to 1?",
-    answerArray: [],
-};
-// Object Question 2
-var questionObject2 = {
-    questionText: "what is the answer to 2?",
-    answerArray:[],
-}
-// Object Question 3
-var questionObject3 = {
-    questionText: "what is the answer to 3?",
-    answerArray:[],
-}
-// Object Question 4
-var questionObject4 = {
-    questionText: "what is the answer to 4?",
-    answerArray:[],
-}
-// Object Question 5
-var questionObject5 = {
-    questionText: "what is the answer to 5?",
-    answerArray:[],
-}
+// Array OF Objects (Each question is an index number)
+var questions = [
+// index 0 = object question 1
+    {
+        questionText: "what is the answer to question 1?",
+        choices: ["wrong answer","right answer","wrong answer","wrong answer"],
+        answer: "2"
+    },
+// index 1 = object question 2
+    {
+        questionText: "what is the answer to question 2?",
+        choices: ["wrong answer", "wrong answer", "right answer"],
+        answer: "3"
+    },
+// index 2 = object question 3
+    {
+        questionText: "what is the answer to question 3?",
+        choices: ["wrong answer", "right answer"],
+        answer: "2"
+    },
+// index 3 = object question 4
+    {
+        questionText: "what is the answer to question 4?",
+        choices: ["wronganswer", "wrong answer", "right answer", "wrong answer"],
+        answer: "3"
+    },
+// index 4 = object question 5
+    {
+        questionText: "what is the answer to question 5?",
+        choices: ["right answer", "wrong answer"],
+        answer: "1"
+    }];
 
 //creating query selectors
-var questionContainer = document.querySelector(".question-container");
-var startButton = document.querySelector(".start");
-
-//Change text of question-container class text to object 1's property "questionText"
-startButton.addEventListener('click' , function(event) {
-    //
-    questionContainer.innerHTML = questionObject.questionText;
-})
+var quiz = document.querySelector("quiz-area"); //all quiz content goes here
+var startButton = document.querySelector(".start"); //start the quiz
+var questionContainer = document.querySelector(".question-container"); //where each question object will go 
+var choices = document.querySelector(".answer-container"); //answer choices 
+var userchoice = []
+var timer;
+var timerCount;

f0dd2c297dcb835da4465343b2dd83fee1c01088
Pillifino <dinodizon073100@gmail.com>
2022-03-17 02:19:40 -0700

    Added all the functions and event listeners for basic quiz functionality. Still missing a way to iterate tthrough each of the questions

--- assets/script.js
+++ assets/script.js
@@ -37,6 +37,91 @@ var startButton = document.querySelector(".start"); //start the quiz
 var questionContainer = document.querySelector(".question-container"); //where each question object will go 
 var choices = document.querySelector(".answer-container"); //answer choices 
 var userchoice = []
+
 var timer;
 var timerCount;
-var score;
+var score = 0;
+var scoreCounter = 0;
+var highScore = 0; //local storage
+var isFinished; false;
+
+//Functions
+//Init Function is called when page loads
+function init(){
+    getHighscore
+}
+// start game
+function startGame() {
+    timerCount = 60;
+    startButton.disabled = true;
+    startTimer()
+}
+
+function finishGame(){
+    quiz.textContent = "You Finished! And Your Score Was:"
+    startButton.disabled = false;
+    setHighscore()
+}
+// Starrt 60 second countdown
+function startTimer() {
+
+}
+//add point when correct
+function addPoint() {
+
+}
+//lose time when incorrect
+function timeLoss() {
+
+}
+//Finish Quiz
+function startTimer() {
+    // Sets timer
+    timer = setInterval(function() {
+      timerCount--;
+      timerElement.textContent = timerCount;
+      if (timerCount >= 0) {
+        // Tests if test is finished
+        if (isFinished && timerCount > 0) {
+          // Clears interval and stops timer
+          clearInterval(timer);
+          setHighscore();
+        }
+      }
+      // Tests if time has run out
+      if (timerCount === 0) {
+        // Clears interval
+        clearInterval(timer);
+        setHighscore();
+      }
+    }, 1000);
+  }
+//set score function
+  function setscore() {
+      score.textContent =  scoreCounter
+      localStorage.setItem("score", scoreCounter)
+
+      if(score > highScore){
+          localStorage.setItem("highScore", scoreCounter)
+      }
+  }
+
+//Event Listeners
+startButton.addEventListener("click", startGame);
+
+// Calls init( so that it fires when page opened
+init();
+
+//Bonuse Add Reset button
+var resetButton = document.querySelector(".reset-button");
+
+function resetGame() {
+    //Resets win and loss counts
+    score = 0; 
+    highScore = 0;
+    // Renders highscore and sets them into client storage
+    setscore()
+    setHighscore()
+}
+// Attaches event listener to button
+resetButton.addEventListener('click', resetGame);
